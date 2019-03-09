36738eba3ec41ef50fadc49bac33f2371add3ef7
Fabian Anguiano <fabiananguiano@gmail.com>
2019-03-09 15:37:34 -0600

    changed box color for landing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,9 +48,7 @@ const Navbar = class extends React.Component {
           </div>
           <div id="navMenu" className="navbar-menu">
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
+            
               <Link className="navbar-item" to="/products">
                 Products
               </Link>
@@ -60,9 +58,7 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
+              
             </div>
             <div className="navbar-end has-text-centered">
               <a
